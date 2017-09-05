0105854f5dfbdfd046f5c25248f58b4570f1ac9e
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-09-05 17:04:33 +0300

    Use ensamble of models

--- model_provider.py
+++ model_provider.py
@@ -1,17 +1,19 @@
 from nameko.extensions import DependencyProvider
 import tensorflow as tf
-from deeppavlov.agents.paraphraser.paraphraser import ParaphraserAgent
+from deeppavlov.agents.paraphraser.paraphraser import EnsembleParaphraserAgent
 import os
+
 
 class Agent(DependencyProvider):
     def __init__(self):
         data_path = os.environ['PARAPHRASER_DATA']
         opt = {
             'fasttext_model': os.path.join(data_path, 'fasttext.bin'),
-            'pretrained_model': os.path.join(data_path, 'paraphraser')
+            'model_files': [os.path.join(data_path, 'models/maxpool_match_%i' % i) for i in range(5)],
+            'datatype': 'test'
+
         }
-        self.agent = ParaphraserAgent(opt)
-        # self.model = ParaphraserModel(opt)
+        self.agent = EnsembleParaphraserAgent(opt)
         self.graph = tf.get_default_graph()
 
     def get_dependency(self, worker_ctx):
