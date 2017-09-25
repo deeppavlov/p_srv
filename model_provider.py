52262f35234d772fb771e8edd040f2cefef1fa45
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-09-25 16:29:27 +0300

    feat: Move all parameters to config file

--- model_provider.py
+++ model_provider.py
@@ -2,17 +2,15 @@ from nameko.extensions import DependencyProvider
 import tensorflow as tf
 from deeppavlov.agents.paraphraser.paraphraser import EnsembleParaphraserAgent
 import os
+import json
 
 
 class Agent(DependencyProvider):
     def __init__(self):
-        data_path = os.environ['PARAPHRASER_DATA']
-        opt = {
-            'fasttext_model': os.path.join(data_path, 'fasttext.bin'),
-            'model_files': [os.path.join(data_path, 'models/sberemb_paraphrasersbertrain_bilstm_woatt_%i' % i) for i in range(1)],
-            'datatype': 'test'
-
-        }
+        config_path = os.path.join(os.environ.setdefault('PARAPHRASER_DATA', '.'), "config.json")
+        with open(config_path, "r") as f:
+            config = json.loads(f.read())
+        opt = config["opt"]
         self.agent = EnsembleParaphraserAgent(opt)
         self.graph = tf.get_default_graph()
 
