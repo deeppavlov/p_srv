1fd6c26b0c573d086d7f4d8f64c95f40cd21b193
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-10-03 17:18:38 +0300

    fixup: Agent initilized more than onece

--- model_provider.py
+++ model_provider.py
@@ -7,18 +7,25 @@ import json
 
 
 class Agent(DependencyProvider):
+    agent = None
+    graph = None
+
     def __init__(self):
-        config_path = os.path.join(os.environ.setdefault('PARAPHRASER_DATA', '.'), "config.json")
-        with open(config_path, "r") as f:
-            config = json.loads(f.read())
-        opt = config["opt"]
-        self.agent = EnsembleParaphraserAgent(opt)
-        self.graph = tf.get_default_graph()
+        if Agent.agent is None:
+            print("Init Agent")
+            config_path = os.path.join(os.environ.setdefault('PARAPHRASER_DATA', '.'), "config.json")
+            with open(config_path, "r") as f:
+                config = json.loads(f.read())
+            opt = config["opt"]
+            Agent.agent = EnsembleParaphraserAgent(opt)
+            Agent.graph = tf.get_default_graph()
+        else:
+            print("Agent already initialized")
 
     def get_dependency(self, worker_ctx):
         return {
-            'agent': self.agent,
-            'graph': self.graph
+            'agent': Agent.agent,
+            'graph': Agent.graph
         }
 
 
